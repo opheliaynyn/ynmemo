797eeb485afdc076d533cbc1cb836c1286d70e67
opheliaynyn <ophelia.y.n@gmail.com>
2018-08-25 16:53:20 +0900

    コメント削除

--- progress/views.py
+++ progress/views.py
@@ -6,17 +6,3 @@ def index(request):
     return render(request,
                   'progress/index.html',
                   {'user': request.user})
-
-"""
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from social_django.models import UserSocialAuth
-
-@login_required
-def index(request):
-    user = UserSocialAuth.objects.get(user_id=request.user.id)
-    pageDic = {
-        'user': user
-    }
-    return render(request, 'progress/index.html', pageDic)
-"""
